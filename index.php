c6d514cb8e8d1d599fe8c5e428b5493f3a44c9cb
bagusnaufalrepo <bagusnaufalabiyyu@student.gunadarma.ac.id>
2025-05-21 19:10:29 +0700

    Menambahkan paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello World</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui tempore culpa, ea nobis, itaque sunt quo aspernatur minus nostrum temporibus optio, delectus at omnis tenetur facere non! Omnis, accusantium vitae.</p>
 </body>
 </html>
